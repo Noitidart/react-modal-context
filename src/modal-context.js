46d71ed2da8576751ebdf95bd629a9419cfebbca
Noitidart <noitidart@gmail.com>
2021-01-11 13:46:57 -0800

    Handle scrollbar pushing content when hidden

--- src/modal-context.js
+++ src/modal-context.js
@@ -50,12 +50,20 @@ export function ModalProvider({ children }) {
         }
       };
       document.addEventListener('keyup', savedHandleEscapePressed.current);
+      document.body.style.overflow = 'hidden';
+      if (isVerticalScrollbarVisible()) {
+        document.body.style.paddingRight = getScrollbarWidth() + 'px';
+      }
       return () => {
         document.removeEventListener('keyup', savedHandleEscapePressed.current);
+        document.body.style.overflow = '';
+        document.body.style.paddingRight = '';
       };
     } else {
       if (savedHandleEscapePressed.current) {
         document.removeEventListener('keyup', savedHandleEscapePressed.current);
+        document.body.style.overflow = '';
+        document.body.style.paddingRight = '';
       }
     }
   }, [opened?.resolve]);
@@ -117,4 +125,36 @@ export function useModal() {
     throw new Error('useModal must be used within a ModalProvider');
   }
   return context;
+}
+
+function isVerticalScrollbarVisible() {
+  return (
+    document.documentElement.scrollHeight >
+    document.documentElement.clientHeight
+  );
+}
+
+let scrollbarWidthCached;
+function getScrollbarWidth() {
+  if (scrollbarWidthCached) {
+    return scrollbarWidthCached;
+  }
+  // Creating invisible container
+  const outer = document.createElement('div');
+  outer.style.visibility = 'hidden';
+  outer.style.overflow = 'scroll'; // forcing scrollbar to appear
+  outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+  document.body.appendChild(outer);
+
+  // Creating inner element and placing it in the container
+  const inner = document.createElement('div');
+  outer.appendChild(inner);
+
+  // Calculating difference between container's full width and the child width
+  const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
+
+  // Removing temporary elements from the DOM
+  outer.parentNode.removeChild(outer);
+
+  return scrollbarWidth;
 }
